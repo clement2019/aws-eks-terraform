0fb93c0b60115cee9b5b871f0a1f36f9c170cdf1
clement2019 <ayeniayokunle@yahoo.co.uk>
2025-01-14 14:15:40 +0100

    fresh files added

--- variable.tf
+++ variable.tf
@@ -1,1 +1,18 @@
 
+# networking
+
+
+variable "public_subnet1_cidr_block" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "my-eks-cluster"
+ }
