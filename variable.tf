2d0ac851206652ebb92228ad1a58b238bbfb58b0
clement2019 <ayeniayokunle@yahoo.co.uk>
2025-01-14 12:55:03 +0100

    fresh files added

--- variable.tf
+++ variable.tf
@@ -1,19 +1,1 @@
- variable "vpc_cidr" {
-  description = "VPC CIDR"
-  type        = string
-}
 
-variable "private_subnet_cidr_block" {
-  type    = string
-  default = []
-}
-
-variable "public_subnet_cidr_block"{
-   type    = string
-  default = []
-}
-
- variable "cluster_name" {
-  type = string
-  default = "my-eks-cluster"
- }
